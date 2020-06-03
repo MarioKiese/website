39496572e8096cc401a21e23852f26a24040ddc7
Mario Kiese <mali.mk73@googlemail.com>
2020-06-04 01:40:57 +0200

    Updates

---
+++ precache-manifest.0a79646b46159c87d43e7f29d1ec6593.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97693d32c009a1a01a008245b202a05",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "d7822e8c49b647f598b9",
+    "url": "/website/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "d4cce0d677fb9c6a09ca",
+    "url": "/website/static/css/main.f2fba9d9.chunk.css"
+  },
+  {
+    "revision": "d7822e8c49b647f598b9",
+    "url": "/website/static/js/2.751bde7d.chunk.js"
+  },
+  {
+    "revision": "e078d673834ef0c088fe44bab9fcc203",
+    "url": "/website/static/js/2.751bde7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4cce0d677fb9c6a09ca",
+    "url": "/website/static/js/main.3fa57f54.chunk.js"
+  },
+  {
+    "revision": "89ed434307d91736e1ef",
+    "url": "/website/static/js/runtime-main.73affe2e.js"
+  },
+  {
+    "revision": "d34f707133ac9212bc09d47b4f12dd94",
+    "url": "/website/static/media/avatar_square.d34f7071.png"
+  }
+]);
