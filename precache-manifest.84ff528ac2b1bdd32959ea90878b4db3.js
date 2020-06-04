dcf82d44b8920c3ce56b41d5cdef98d6b5bb18c3
Mario Kiese <mali.mk73@googlemail.com>
2020-06-04 02:04:34 +0200

    Updates

---
+++ precache-manifest.84ff528ac2b1bdd32959ea90878b4db3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc877366598d516c6a7fa940056221e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7822e8c49b647f598b9",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "3a08efe571fb94acc932",
+    "url": "/static/css/main.f2fba9d9.chunk.css"
+  },
+  {
+    "revision": "d7822e8c49b647f598b9",
+    "url": "/static/js/2.751bde7d.chunk.js"
+  },
+  {
+    "revision": "e078d673834ef0c088fe44bab9fcc203",
+    "url": "/static/js/2.751bde7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a08efe571fb94acc932",
+    "url": "/static/js/main.9f44993e.chunk.js"
+  },
+  {
+    "revision": "5fce37acc170e4c6fca6",
+    "url": "/static/js/runtime-main.27b64fcf.js"
+  },
+  {
+    "revision": "d34f707133ac9212bc09d47b4f12dd94",
+    "url": "/static/media/avatar_square.d34f7071.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.292318b3e2a07e7d24cf1ad0d3bbbe35.js"
+  "/precache-manifest.84ff528ac2b1bdd32959ea90878b4db3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
